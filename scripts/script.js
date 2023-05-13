8ae37cb234c734dcf731b478240886b343866eec
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-05-13 11:17:11 +0300

    Fetching Completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,49 @@
-// async function getSecret() {
-//   const res = await fetch(
-//     "https://random-word-api.herokuapp.com/word?number=1"
-//   );
-//   const data = await res.json();
-//   secretWord = data[0];
-// }
-
-let secretWord = "ankara";
+async function getSecret() {
+  const res = await fetch(
+    "https://random-word-api.herokuapp.com/word?number=1"
+  );
+  const data = await res.json();
+  return data[0];
+}
 const buttons = document.querySelectorAll(".alphabet");
 const lives = document.querySelector(".lives");
 const dis = document.querySelector(".display");
 const start = document.querySelector("#start");
 const reset = document.querySelector("#reset");
-
-//split the word
-let newWord = secretWord.split("");
-let display = [];
+let display;
 let guess = 10;
 let count = 0;
+let newWord;
+let secretWord;
 
 //Starting the game
 start.addEventListener("click", print);
 function print() {
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].disabled = false;
-  }
-  lives.innerHTML = `Your lives: ${guess}`;
-  for (let i = 0; i < newWord.length; i++) {
-    display[i] = "_ ";
-  }
-  if (!dis.innerHTML) {
+  getSecret().then((word) => {
+    console.log(word);
+    secretWord = word;
+    display = [];
+    dis.innerHTML = "";
+    lives.innerHTML = ``;
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+      buttons[i].classList = "alphabet";
+      guess = 10;
+    }
+    //split the word
+    newWord = secretWord.split("");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = false;
+    }
+    lives.innerHTML = `Your lives: ${guess}`;
+    for (let i = 0; i < newWord.length; i++) {
+      display[i] = "_ ";
+    }
+    // if (!dis.innerHTML) {
     dis.innerHTML = display.join("");
-  }
+    // }
+  });
 }
-reset.addEventListener("click", () => {
-  dis.innerHTML = "";
-  lives.innerHTML = ``;
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].disabled = true;
-    buttons[i].classList = "alphabet";
-    guess = 10;
-  }
-});
 
 //take letter from user
 for (let i = 0; i < buttons.length; i++) {
@@ -71,11 +73,10 @@ function letterGuess(e) {
     dis.innerHTML = display.join("");
     lives.innerHTML = `Your lives: ${guess}`;
     if (guess == 0) {
-      lives.innerHTML = `Game Over!`;
+      lives.innerHTML = `Game Over! the word was "${secretWord}"`;
       for (let i = 0; i < buttons.length; i++) {
         buttons[i].disabled = true;
       }
     }
-    console.log(guess);
   }
 }
